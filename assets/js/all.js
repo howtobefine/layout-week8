87673e8cae2a5f7250efd28a5744311876f78d89
howtobefine <b4808697b@gmail.com>
2022-08-30 20:28:21 +0800

    Update 2022-08-30T12:28:18.680Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,17 @@
 "use strict";
+"use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  centeredSlides: true,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+});
+$('.row').imagesLoaded().progress(function () {
+  $('.row').masonry();
 });
 //# sourceMappingURL=all.js.map
