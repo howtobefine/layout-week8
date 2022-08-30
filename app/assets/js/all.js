8d705c509fe1e342c865cbdc06b18d3bba83aa9c
howtobefine <b4808697b@gmail.com>
2022-08-30 23:10:03 +0800

    layout修改

---
+++ app/assets/js/all.js
@@ -0,0 +1,13 @@
+$('.back').click(function (e) { 
+    e.preventDefault();
+    $('#navbarSearch').toggleClass('show');
+    $('.navbar-icon').removeClass('d-none')
+    $('.navbar-icon').addClass('d-block')
+    $('.logo').show();
+});
+
+$('.search').click(function (e) { 
+    e.preventDefault();
+    $('.navbar-icon').toggleClass('d-none');
+    $('.logo').hide();
+});
