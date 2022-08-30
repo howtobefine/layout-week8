10c3abd799193d03da48116c6caf3ce1e71371b9
howtobefine <b4808697b@gmail.com>
2022-08-30 23:10:25 +0800

    Update 2022-08-30T15:10:23.690Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,4 +1,17 @@
 "use strict";
+
+$('.back').click(function (e) {
+  e.preventDefault();
+  $('#navbarSearch').toggleClass('show');
+  $('.navbar-icon').removeClass('d-none');
+  $('.navbar-icon').addClass('d-block');
+  $('.logo').show();
+});
+$('.search').click(function (e) {
+  e.preventDefault();
+  $('.navbar-icon').toggleClass('d-none');
+  $('.logo').hide();
+});
 "use strict";
 
 var swiper = new Swiper('.swiper', {
