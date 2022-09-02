3f11859fcc2bf0720f00ff5954c4a179ea066d17
howtobefine <b4808697b@gmail.com>
2022-09-02 20:16:38 +0800

    maybe final

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,8 +1,8 @@
 $('.back').click(function (e) { 
     e.preventDefault();
     $('#navbarSearch').toggleClass('show');
-    $('.navbar-icon').removeClass('d-none')
-    $('.navbar-icon').addClass('d-block')
+    $('.navbar-icon').removeClass('d-none');
+    $('.navbar-icon').addClass('d-block');
     $('.logo').show();
 });
 
@@ -10,4 +10,9 @@ $('.search').click(function (e) {
     e.preventDefault();
     $('.navbar-icon').toggleClass('d-none');
     $('.logo').hide();
+});
+
+$('.btn-plus').click(function (e) { 
+    e.preventDefault();
+    $(this).toggleClass('active');
 });
--- app/assets/js/vendors.js
+++ app/assets/js/vendors.js
@@ -3,12 +3,31 @@ var swiper = new Swiper('.swiper', {
     spaceBetween: 24,
     centeredSlides: true,
     loop: true,
+    autoplay: true,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
 });
 
-$('.row').imagesLoaded().progress(function () {
-    $('.row').masonry();
+var swiperOthers = new Swiper('.swiperOthers', {
+    slidesPerView: 2,
+    spaceBetween: 24,
+    loop: true,
+    autoplay: true,
+    breakpoints: {
+        992: {
+            slidesPerView: 4
+        }
+    },
+    pagination: {
+        el: '.swiperOthers-pagination',
+        clickable: true,
+    },
+});
+
+
+
+$('.row-masonry').imagesLoaded().progress(function () {
+    $('.row-masonry').masonry();
 });
